a81491b4b21f48b6695c51d67ffef3e9066746ea
vldslvvasilec <135536625+vldslvvasilec@users.noreply.github.com>
2023-11-06 14:05:30 +0100

    version1.0
    version1.0

---
+++ js/_lang.js
@@ -0,0 +1,78 @@
+const Title = {
+    title: {
+        ru: "Резюме Владислав Шапошник",
+        en: "Rezume Vladyslav Shaposhnyk",
+        ua: "Резюме Владислав Шапошник",
+        cz: "Životopis Vladislav Šapošník",
+    },
+};
+const langArr = {
+    lsg_header_home: { ru: "Главная", en: "Home", ua: "Головна", cz: "Domov" },
+    lsg_header_skills: {
+        ru: "Навыки",
+        en: "Skills",
+        ua: "Вміння",
+        cz: "Dovednosti",
+    },
+    lsg_header_portfolio: {
+        ru: "Портфолио",
+        en: "Portfolio",
+        ua: "Портфоліо",
+        cz: "Portfolio",
+    },
+    lsg_header_about_me: {
+        ru: "Обо мне",
+        en: "About Me",
+        ua: "Про мене",
+        cz: "O mně",
+    },
+    lsg_header_work: { ru: "Работо", en: "Work", ua: "Робота", cz: "Práce" },
+    lsg_header_education: {
+        ru: "Образование",
+        en: "Education",
+        ua: "Освіта",
+        cz: "Vzdělání",
+    },
+    lsg_header_languages: {
+        ru: "Языки",
+        en: "Languages",
+        ua: "Мови",
+        cz: "Jazyky",
+    },
+    lsg_header_hobbies: {
+        ru: "Хобби",
+        en: "Hobbies",
+        ua: "Хобі",
+        cz: "Záliby",
+    },
+    lsg_main_velcome: {
+        ru: "Добро пожаловать на мою страницу.",
+        en: "Welcome to my page.",
+        ua: "Ласкаво просимо на мою сторінку.",
+        cz: "Vítejte na mé stránce.",
+    },
+    lsg_main_my_name: {
+        ru: "Я Владислав Шапошник",
+        en: "I'm Vladislav Shapošnik",
+        ua: "Мене звати Владислав Шапошник",
+        cz: "Jmenuji se Vladyslav Shaposhnyk",
+    },
+    lsg_main_search_work1: {
+        ru: "Ищу работу в сфере IT технологий.",
+        en: "I'm looking for a job in the field of IT technologies.",
+        ua: "Шукаю роботу в галузі ІТ-технологій.",
+        cz: "Hledám práci v oblasti IT technologií.",
+    },
+    lsg_main_search_work2: {
+        ru: "С подробной информацией обо мне можете ознакомиться на соответствующих вкладках этой страницы.",
+        en: "You can find detailed information about me on the respective tabs of this page.",
+        ua: "Докладну інформацію про мене ви можете знайти на відповідних вкладках цієї сторінки.",
+        cz: "Podrobné informace o mně můžete najít na příslušných kartách této stránky.",
+    },
+    lsg_main_search_work3: {
+        ru: "Если у Вас есть для меня предложения, пожалуйста свяжитесь со мной.",
+        en: "If you have any offers for me, please get in touch.",
+        ua: "Якщо у вас є пропозиції для мене, будь ласка, зв'яжіться зі мною.",
+        cz: "Pokud máte pro mě nabídky, prosím, kontaktujte mě.",
+    },
+};
