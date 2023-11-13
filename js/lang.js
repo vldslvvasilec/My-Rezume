e3622d52ec1ceb5baf34883b2f261d9c0da9c88d
vldslvvasilec <135536625+vldslvvasilec@users.noreply.github.com>
2023-11-13 08:02:02 +0100

    Update lang.js

--- js/lang.js
+++ js/lang.js
@@ -11,13 +11,13 @@ const langArr = {
     lsg_header_skills: {
         ru: "Навыки",
         en: "Skills",
-        ua: "Вміння",
+        ua: "Вм&iacute;ння",
         cz: "Dovednosti",
     },
     lsg_header_portfolio: {
         ru: "Портфолио",
         en: "Portfolio",
-        ua: "Портфоліо",
+        ua: "Портфол&iacute;о",
         cz: "Portfolio",
     },
     lsg_header_about_me: {
@@ -30,7 +30,7 @@ const langArr = {
     lsg_header_education: {
         ru: "Образование",
         en: "Education",
-        ua: "Освіта",
+        ua: "Осв&iacute;та",
         cz: "Vzdělání",
     },
     lsg_header_languages: {
@@ -42,7 +42,7 @@ const langArr = {
     lsg_header_hobbies: {
         ru: "Хобби",
         en: "Hobbies",
-        ua: "Хобі",
+        ua: "Хоб&iacute;",
         cz: "Záliby",
     },
     lsg_header_Characteristic: {
@@ -54,7 +54,7 @@ const langArr = {
     lsg_main_velcome: {
         ru: "Добро пожаловать на мою страницу.",
         en: "Welcome to my page.",
-        ua: "Ласкаво просимо на мою сторінку.",
+        ua: "Ласкаво просимо на мою стор&iacute;нку.",
         cz: "Vítejte na mé stránce.",
     },
     lsg_main_my_name: {
@@ -66,51 +66,51 @@ const langArr = {
     lsg_main_search_work1: {
         ru: "Ищу работу в сфере IT технологий.",
         en: "I'm looking for a job in the field of IT technologies.",
-        ua: "Шукаю роботу в галузі ІТ-технологій.",
+        ua: "Шукаю роботу в галуз&iacute; &iacute;Т-технолог&iacute;й.",
         cz: "Hledám práci v oblasti IT technologií.",
     },
     lsg_main_search_work2: {
         ru: "С подробной информацией обо мне можете ознакомиться на соответствующих вкладках этой страницы.",
         en: "You can find detailed information about me on the respective tabs of this page.",
-        ua: "Докладну інформацію про мене ви можете знайти на відповідних вкладках цієї сторінки.",
+        ua: "Докладну &iacute;нформац&iacute;ю про мене ви можете знайти на в&iacute;дпов&iacute;дних вкладках ц&iacute;єї стор&iacute;нки.",
         cz: "Podrobné informace o mně můžete najít na příslušných kartách této stránky.",
     },
     lsg_main_search_work3: {
         ru: "Если у Вас есть для меня предложения, пожалуйста свяжитесь со мной.",
         en: "If you have any offers for me, please get in touch.",
-        ua: "Якщо у вас є пропозиції для мене, будь ласка, зв'яжіться зі мною.",
+        ua: "Якщо у вас є пропозиц&iacute;ї для мене, будь ласка, зв'яж&iacute;ться з&iacute; мною.",
         cz: "Pokud máte pro mě nabídky, prosím, kontaktujte mě.",
     },
     lsg_main_character_title: {
         ru: "Самооценка",
         en: "Self-Assessment",
-        ua: "Самооцінка",
+        ua: "Самооц&iacute;нка",
         cz: "Sebehodnocení",
     },
     lsg_main_character_text: {
         ru: "Опытный специалист с фокусом на достижении результатов. Мои сильные стороны включают в себя стратегическое мышление, аналитические навыки и умение эффективно работать в команде. Настойчивость – это ключевая часть моего подхода к работе: я готов преодолевать трудности и находить решения, несмотря на сложности. Я постоянно стремлюсь к профессиональному развитию и открыт для новых вызовов. Стремлюсь к достижению высоких стандартов качества в своей работе, а также готов к обучению и внесению положительного вклада в успех команды.",
 
         en: "Experienced professional with a focus on achieving results. My strengths include strategic thinking, analytical skills, and the ability to work effectively in a team. Persistence is a key part of my approach to work: I am ready to overcome challenges and find solutions despite difficulties. I am constantly striving for professional development and open to new challenges. I aim to achieve high standards of quality in my work and am ready to learn and contribute to the team's success.",
-        ua: "Досвідчений фахівець з фокусом на досягненні результатів. Мої сильні сторони включають стратегічне мислення, аналітичні навички та здатність ефективно працювати в команді. Наполегливість - ключова частина мого підходу до роботи: я готовий подолати труднощі та знаходити рішення незважаючи на складнощі. Я постійно прагну до професійного розвитку та відкритий для нових викликів. Мета - досягти високих стандартів якості в своїй роботі та готовий вчитися та вносити позитивний внесок у успіх команди.",
+        ua: "Досв&iacute;дчений фах&iacute;вець з фокусом на досягненн&iacute; результат&iacute;в. Мої сильн&iacute; сторони включають стратег&iacute;чне мислення, анал&iacute;тичн&iacute; навички та здатн&iacute;сть ефективно працювати в команд&iacute;. Наполеглив&iacute;сть - ключова частина мого п&iacute;дходу до роботи: я готовий подолати труднощ&iacute; та знаходити р&iacute;шення незважаючи на складнощ&iacute;. Я пост&iacute;йно прагну до профес&iacute;йного розвитку та в&iacute;дкритий для нових виклик&iacute;в. Мета - досягти високих стандарт&iacute;в якост&iacute; в своїй робот&iacute; та готовий вчитися та вносити позитивний внесок у усп&iacute;х команди.",
         cz: "Zkušený odborník s důrazem na dosahování výsledků. Mezi mé silné stránky patří strategické myšlení, analytické dovednosti a schopnost efektivně pracovat v týmu. Vytrvalost je klíčovou součástí mého přístupu k práci: jsem připraven překonávat obtíže a hledat řešení navzdory složitostem. Neustále se snažím o profesionální rozvoj a jsem otevřen novým výzvám. Usiluji o dosažení vysokých standardů kvality ve své práci a jsem připraven se učit a přispívat k úspěchu týmu.",
     },
     lsg_main_languages: {
         ru: "Языковые навыки",
         en: "Language Skills",
-        ua: "Мовні навички",
+        ua: "Мовн&iacute; навички",
         cz: " Jazykové dovednosti",
     },
     lsg_main_english: {
         ru: "Английский",
         en: "English",
-        ua: "Англійська",
+        ua: "Англ&iacute;йська",
         cz: "Angličtina",
     },
     lsg_main_cheh: { ru: "Чешский", en: "Czech", ua: "Чеська", cz: "Čeština" },
     lsg_main_rus: {
         ru: "Русский",
         en: "Russian",
-        ua: "Російська",
+        ua: "Рос&iacute;йська",
         cz: "Ruština",
     },
     lsg_main_ukranian: {
@@ -122,13 +122,13 @@ const langArr = {
     lsg_main_hobies_title: {
         ru: "Увлечения",
         en: "Hobbies",
-        ua: " Хобі",
+        ua: " Хоб&iacute;",
         cz: "Koníčky",
     },
     lsg_main_hobies_text: {
         ru: "В свободное время я с удовольствием занимаюсь постоянным обучением, всегда стремясь к расширению своих знаний и навыков. Люблю проводить время на пеших прогулках, используя их как возможность для анализа и размышлений. Меня привлекает красота природы, особенно леса и гор, что приносит мне не только удовольствие, но и вдохновение.",
         en: "In my free time, I enjoy continuous learning, always striving to expand my knowledge and skills. I find solace in taking long walks, using them as an opportunity for analysis and contemplation. The beauty of nature, particularly forests and mountains, captivates me, providing not only enjoyment but also inspiration.",
-        ua: "У вільний час я з задоволенням займаюсь постійним навчанням, завжди прагнучи розширити свої знання та навички. Обожнюю проводити час на довгих прогулянках, використовуючи їх як можливість для аналізу і роздумів. Краса природи, зокрема лісів і гір, захоплює мене, надаючи не лише задоволення, а й натхнення.",
+        ua: "У в&iacute;льний час я з задоволенням займаюсь пост&iacute;йним навчанням, завжди прагнучи розширити свої знання та навички. Обожнюю проводити час на довгих прогулянках, використовуючи їх як можлив&iacute;сть для анал&iacute;зу &iacute; роздум&iacute;в. Краса природи, зокрема л&iacute;с&iacute;в &iacute; г&iacute;р, захоплює мене, надаючи не лише задоволення, а й натхнення.",
         cz: "Ve svém volném čase rád pokračuji ve vzdělávání, neustále se snažíc rozšiřovat své znalosti a dovednosti. Rád trávím čas dlouhými procházkami, které využívám jako příležitost k analýze a zamyšlení. Krása přírody, zejména lesů a hor, mě pohlcuje a přináší nejen radost, ale také inspiraci.",
     },
 };
