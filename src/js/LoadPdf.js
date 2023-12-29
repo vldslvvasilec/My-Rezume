4efb325f31d3b2f975ba9afb9a99f047c3ced5a2
vldslvvasilec <135536625+vldslvvasilec@users.noreply.github.com>
2023-12-29 09:22:04 +0100

    viteMyRezume
    viteMyRezume

---
+++ src/js/LoadPdf.js
@@ -0,0 +1,40 @@
+const PdfModall = document.querySelector(".LoadPdfWindow");
+const PdfNextText = document.querySelector(".LoadPdfWindow_lang");
+const PdfButtonsMass = document.querySelectorAll("li.PdfElem");
+const PdfButtonOk = document.querySelector(".LoadPdfButtonsOK");
+const PdfButtonCancel = document.querySelector(".LoadPdfButtonsCancel");
+let PdfUserLang;
+let PdfFileUrl = `/src/pdf/Rezume${PdfUserLang}.pdf`;
+let PdfFileName = `Rezume${PdfUserLang}.pdf`;
+// Закрытие модалки PDF, открытие экрана
+const ClosePdfWindow = () => {
+    PdfModall.classList.remove("PdfShowModal");
+    PdfModall.classList.add("PdfCloseModal");
+    Body.classList.remove("translucentBg");
+};
+PdfButtonsMass.forEach((PdfButton) => {
+    // Открытие модалки PDF, затемнение экрана, добавление текста lang
+    PdfButton.addEventListener("click", function () {
+        const textOfButton = PdfButton.getAttribute("value");
+        PdfNextText.innerText = textOfButton;
+        PdfUserLang = textOfButton;
+        Body.classList.add("translucentBg");
+        PdfModall.classList.remove("PdfCloseModal");
+        PdfModall.classList.add("PdfShowModal");
+    });
+});
+// Закрытие модалки PDF, открытие экрана при клике Cancel
+PdfButtonCancel.addEventListener("click", function () {
+    ClosePdfWindow();
+});
+// Закрытие модалки PDF, открытие экрана при клике OK
+PdfButtonOk.addEventListener("click", function () {
+    // Добавление ссылки, скачивание файла от Lang, удаление ссылки после скачивания, закрытие окна
+    let a = document.createElement("a");
+    a.href = `/src/assets/pdf/Rezume${PdfUserLang}.pdf`;
+    a.download = `Rezume${PdfUserLang}.pdf`;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+    ClosePdfWindow();
+});
